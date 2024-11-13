e395a6405f2442faaaf0c802f3a6580add2ceadb
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-11-13 11:21:48 -0500

    Update test_doc_module.py

--- tests/Unit/Models/Entities/test_doc_module.py
+++ tests/Unit/Models/Entities/test_doc_module.py
@@ -9,7 +9,7 @@ def test_normalize() -> None:
     cache = ModuleCache(0xB5, 0x08F3)
     cache.module_cookie = 0xB81C
     cache.misc = [[0x0316, 0, 0x0123, 0x88],
-                  [-1, 8], 24, "00000000", 1, 0]
+                  [-1, 8], 24, 0, [1, "00000000"]]
     guid = uuid.UUID('0002081900000000C000000000000046')
     cache.guid = [guid]
 
