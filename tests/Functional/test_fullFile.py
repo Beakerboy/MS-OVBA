39a5592889a2743dcc72a82b0eec3bf0aff00dbb
Kevin Nowaczyk <beakerboy99@yahoo.com>
2025-12-31 10:10:02 -0500

    Refactor import statements for ms_ovba module

--- tests/Functional/test_fullFile.py
+++ tests/Functional/test_fullFile.py
@@ -7,14 +7,14 @@ from ms_cfb.ole_file import OleFile
 from ms_dtyp.filetime import Filetime
 from ms_ovba_compression.ms_ovba import MsOvba
 from ms_pcode_assembler.module_cache import ModuleCache
-from vbaproject_compiler.vbaProject import VbaProject
-from vbaproject_compiler.Models.Entities.doc_module import DocModule
-from vbaproject_compiler.Models.Entities.std_module import StdModule
-from vbaproject_compiler.Models.Entities.reference_record import (
+from ms_ovba.vbaProject import VbaProject
+from ms_ovba.Models.Entities.doc_module import DocModule
+from ms_ovba.Models.Entities.std_module import StdModule
+from ms_ovba.Models.Entities.reference_record import (
     ReferenceRecord
 )
-from vbaproject_compiler.Models.Fields.libid_reference import LibidReference
-from vbaproject_compiler.Views.project_ole_file import ProjectOleFile
+from ms_ovba.Models.Fields.libid_reference import LibidReference
+from ms_ovba.Views.project_ole_file import ProjectOleFile
 from typing import Type, TypeVar
 
 
