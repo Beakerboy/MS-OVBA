5e929e3d7d8ae3a8eaf8de29ca33b1a9aacf7e46
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-02-26 14:38:11 -0500

    Update project_reference.py

--- src/ms_ovba/Models/Fields/project_reference.py
+++ src/ms_ovba/Models/Fields/project_reference.py
@@ -5,19 +5,32 @@ T = TypeVar('T', bound='ProjectReference')
 
 
 class ProjectReference():
+    """
+    Specifies the identifier of a VBA project.
+    """
     def __init__(self: T, project_path: str, embedded: bool = true) -> None:
         self.project_path = project_path
         self.embedded = embedded
 
     def __str__(self):
+        return self._header() + \
+            str(self.project_path)
+
+    def __len__(self):
+        return len(str(self))
+
+    def _header(self: T): str
         project_kind = 0x41
         if not(self._is_windows_path(project_path)):
             project_kind += 2
         if self.embedded:
             project_kind += 1
         return "*\\" + \
-            chr(self.project_kind) + \
-            str(self.project_path)
+            chr(self.project_kind)
 
-    def __len__(self):
-        return len(str(self))
+    def relative_to(self: T, path: str):
+        """
+        Return the path relative to another path.
+        """
+        return self._header() + \
+            str(self.project_path)
