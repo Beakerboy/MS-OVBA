79e53281983f5664dce0afef768b4253074409e6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-02-26 14:42:47 -0500

    Update project_reference.py

--- src/ms_ovba/Models/Fields/project_reference.py
+++ src/ms_ovba/Models/Fields/project_reference.py
@@ -12,14 +12,14 @@ class ProjectReference():
         self.project_path = project_path
         self.embedded = embedded
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self._header() + \
             str(self.project_path)
 
     def __len__(self):
         return len(str(self))
 
-    def _header(self: T):
+    def _header(self: T) -> str:
         project_kind = 0x41
         if not(self._is_windows_path(project_path)):
             project_kind += 2
@@ -28,7 +28,7 @@ class ProjectReference():
         return "*\\" + \
             chr(self.project_kind)
 
-    def relative_to(self: T, path: str):
+    def relative_to(self: T, path: str) -> str:
         """
         Return the path relative to another path.
         """
