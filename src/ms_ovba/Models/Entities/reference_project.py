86b27dbb3adc3df5af1785efa920e11dac66526f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-02-27 08:38:01 -0500

    Update reference_project.py

--- src/ms_ovba/Models/Entities/reference_project.py
+++ src/ms_ovba/Models/Entities/reference_project.py
@@ -18,12 +18,16 @@ class ReferenceProject():
         self._ref = ref
 
     def pack(self: T, codepage_name: str, endien: str) -> bytes:
-        size_of_libid_absolute = len(self._ref)
-        size_of_libid_relative = len(self._ref.relative_to(""))
-        format = "HII" + str(size_of_libid_absolute) + "sI" + str(size_of_libid_relative) + "sIH"
-        ref_str = str(self._ref).encode(self._codepage_name)
-        ref_str_rel = str(self._ref.relative_to("")).encode(self._codepage_name)
-        ref_project = PackedData(format, 0x000E, size_of_libid_absolute + size_of_libid_relative + 20,
-                                    size_of_libid_absolute, ref_str, size_of_libid_relative, ref_str_rel, 0x65BE0257, 0x0017)
+        cp_name = self._codepage_name
+        libid_abs_size = len(self._ref)
+        libid_rel_size = len(self._ref.relative_to(""))
+        format = ("HII" + str(libid_abs_size) + "sI" +
+                  str(libid_rel_size) + "sIH")
+        ref_str = str(self._ref).encode(cp_name)
+        ref_str_rel = str(self._ref.relative_to("")).encode(cp_name)
+        ref_project = PackedData(format, 0x000E,
+                                 libid_abs_size + libid_rel_size + 20,
+                                 libid_abs_size, ref_str, libid_rel_size, 
+                                 ref_str_rel, 0x65BE0257, 0x0017)
 
-        return (ref_project.pack(codepage_name, endien))
+        return (ref_project.pack(cp_name, endien))
