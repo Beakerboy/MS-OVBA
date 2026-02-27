af43a4af1682f8dddbc185203a147c7004a0b1a9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-02-27 13:53:17 -0500

    Update test_project_reference.py

--- tests/Unit/Models/Fields/test_project_reference.py
+++ tests/Unit/Models/Fields/test_project_reference.py
@@ -2,17 +2,17 @@ from ms_ovba.Models.Fields.project_reference import ProjectReference
 
 
 def test_constructor() -> None:
-    ref = ProjectReference("C:\Example Path\Example-ReferencedProject.xls")
+    ref = ProjectReference("C:\\Example Path\\Example-ReferencedProject.xls")
     assert isinstance(module, ReferenceProject)
 
 def test_constructor() -> None:
-    ref = ProjectReference("C:/", False)
+    ref = ProjectReference("C:\\", False)
     assert isinstance(module, ReferenceProject)
 
 def test_len() -> None:
-    ref = ProjectReference("C:\Example Path\Example-ReferencedProject.xls")
+    ref = ProjectReference("C:\\Example Path\\Example-ReferencedProject.xls")
     assert len(ref) == 48
 
 def test_str() -> None:
-    ref = ProjectReference("C:\Example Path\Example-ReferencedProject.xls")
-    assert str(ref) == "*\CC:\Example Path\Example-ReferencedProject.xls"
+    ref = ProjectReference("C:\\Example Path\\Example-ReferencedProject.xls")
+    assert str(ref) == "*\\CC:\\Example Path\\Example-ReferencedProject.xls"
