9dfc4b65ef5d8dc4a1400f9a85a4317e2efadfd4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-02-27 13:52:37 -0500

    Update project_reference.py

--- src/ms_ovba/Models/Fields/project_reference.py
+++ src/ms_ovba/Models/Fields/project_reference.py
@@ -30,7 +30,7 @@ class ProjectReference():
         Strip off the path and just return the file.
         """
         # Find last '\'
-        pos = self._project_path.rfind('\')
+        pos = self._project_path.rfind('\\')
         
         rel_path = self._project_path[pos + 1:]
         return ProjectReference(rel_path, self._embedded)
