5469b92bbaba5aa13ed609245c524c81f2dc1d9e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-02-27 09:01:00 -0500

    Create reference.py

---
+++ src/ms_ovba/Models/Entities/reference.py
@@ -0,0 +1,23 @@
+from ms_ovba.Models.Fields.project_reference import ProjectReference
+from ms_ovba.Models.Fields.packed_data import PackedData
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='Reference')
+
+
+class Reference():
+    """
+    2.3.4.2.2.1 REFERENCE Record
+    """
+    def __init__(self: T, codepage_name: str,
+                 ref: ReferenceType, name: ReferenceName = None) -> None:
+        # is self._codepage_name even needed?
+        self._codepage_name = codepage_name
+        self._ref = ref
+        self._name = name
+
+    def pack(self: T, cp_name: str, endien: str) -> bytes:
+        name = self._name
+        pack_name = name.pack(cp_name, endien) if not name is None else ""
+        return pack_name + self._ref.pack(cp_name, endien)
