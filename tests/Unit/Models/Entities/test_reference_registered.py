7aacc9dcecc47d805d329891d4ab520154fea549
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-02-27 14:37:38 -0500

    Create test_reference_registered.py

---
+++ tests/Unit/Models/Entities/test_reference_registered.py
@@ -0,0 +1,42 @@
+import uuid
+from ms_ovba.Models.Entities.reference_registered import ReferenceRegistered
+from ms_ovba.Models.Fields.libid_reference import LibidReference
+
+
+def test_constructor() -> None:
+    guid = uuid.UUID('0002043000000000C000000000000046')
+    ref = ref = LibidReference(
+        guid,
+        "2.0",
+        "0",
+        "C:\\Windows\\System32\\stdole2.tlb",
+        "OLE Automation"
+    )
+    module = ReferenceProject("cp1", ref)
+    assert isinstance(module, ReferenceRegistered)
+
+
+def test_pack() -> None:
+    guid = uuid.UUID('0002043000000000C000000000000046')
+    ref = ref = LibidReference(
+        guid,
+        "2.0",
+        "0",
+        "C:\\Windows\\System32\\stdole2.tlb",
+        "OLE Automation"
+    )
+
+    expected_hex = ("0D 00 68 00 00 00 5E 00 00 00 2A 5C 00 00 00 00",
+                    "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                    "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                    "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                    "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                    "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                    "00 00 00 00 00 00 00 00 00 00 00 00 00 00",
+                   )
+    expected = bytes.fromhex(" ".join(expected_hex))
+    codepage = 0x04E4
+    codepage_name = "cp" + str(codepage)
+    ref_reg = ReferenceRegistered(codepage_name, ref)
+    results = ref_reg.pack(codepage_name, 'little')
+    assert results == expected
