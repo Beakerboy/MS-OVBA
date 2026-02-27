a63d1f67615f07da9fab065d4ad210c9d288b287
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-02-27 10:05:24 -0500

    Update reference_registered.py

--- src/ms_ovba/Models/Entities/reference_registered.py
+++ src/ms_ovba/Models/Entities/reference_registered.py
@@ -16,18 +16,16 @@ class ReferenceRegistered(ReferenceType):
     Specifies a reference to an Automation type library.
     """
     def __init__(self: T, codepage_name: str,
-                 name: str, libid_ref: LibidReference) -> None:
+                 libid_ref: LibidReference) -> None:
         # is self._codepage_name even needed?
         self._codepage_name = codepage_name
-        self._refname = DoubleEncodedString([0x0016, 0x003E], name)
         self._libid_ref = libid_ref
 
-    def pack(self: T, codepage_name: str, endien: str) -> bytes:
+    def pack(self: T, cp_name: str, endien: str) -> bytes:
         strlen = len(self._libid_ref)
         format = "HII" + str(strlen) + "sIH"
-        lib_str = str(self._libid_ref).encode(self._codepage_name)
+        lib_str = str(self._libid_ref).encode(cp_name)
         ref_registered = PackedData(format, 0x000D, strlen + 10,
                                     strlen, lib_str, 0, 0)
 
-        return (self._refname.pack(codepage_name, endien)
-                + ref_registered.pack(codepage_name, endien))
+        return ref_registered.pack(cp_name, endien)
