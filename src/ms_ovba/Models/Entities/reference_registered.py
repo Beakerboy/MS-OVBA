5a24efbaf88983982af5671bf41778ac960fe4e7
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-02-27 11:08:41 -0500

    Update reference_registered.py

--- src/ms_ovba/Models/Entities/reference_registered.py
+++ src/ms_ovba/Models/Entities/reference_registered.py
@@ -42,8 +42,8 @@ class ReferenceRegistered(ReferenceType):
         start = end
         end = end + 4
         recordsize = bytes[start:end]
-        if len(data) != recordsize + 6:
-            # raise a warning
+        # if len(data) != recordsize + 6:
+        # raise a warning
 
         start = end
         end = end + 4
@@ -54,14 +54,14 @@ class ReferenceRegistered(ReferenceType):
         start = end
         end = end + 4
         reserved1 = bytes[start:end]
-        if reserved1 != 0:
-            # raise a warning
+        # if reserved1 != 0:
+        # raise a warning
 
         start = end
         end = end + 2
         reserved2 = bytes[start:end]
-        if reserved2 != 0:
-            # raise a warning
+        # if reserved2 != 0:
+        # raise a warning
         
         libid_ref = LibidReference.create(libid_ref_bytes)
         return ReferenceRegistered("", libid_ref)
