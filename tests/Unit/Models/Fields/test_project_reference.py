03ebaa715fb63703a995cf0009fa0f7bf7902f38
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-02-27 13:54:25 -0500

    Update test_project_reference.py

--- tests/Unit/Models/Fields/test_project_reference.py
+++ tests/Unit/Models/Fields/test_project_reference.py
@@ -5,13 +5,16 @@ def test_constructor() -> None:
     ref = ProjectReference("C:\\Example Path\\Example-ReferencedProject.xls")
     assert isinstance(module, ReferenceProject)
 
+
 def test_constructor() -> None:
     ref = ProjectReference("C:\\", False)
     assert isinstance(module, ReferenceProject)
 
+
 def test_len() -> None:
     ref = ProjectReference("C:\\Example Path\\Example-ReferencedProject.xls")
     assert len(ref) == 48
+
 
 def test_str() -> None:
     ref = ProjectReference("C:\\Example Path\\Example-ReferencedProject.xls")
