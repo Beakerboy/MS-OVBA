7883a201c8903e960293d701f6522c71ebcd0dd4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-02-27 12:55:54 -0500

    Update test_reference_project.py

--- tests/Unit/Models/Entities/test_reference_project.py
+++ tests/Unit/Models/Entities/test_reference_project.py
@@ -3,7 +3,17 @@ from ms_ovba.Models.Fields.project_reference import ProjectReference
 
 
 def test_constructor() -> None:
-    ref = ProjectReference("")
+    ref = ProjectReference("C:/")
     module = ReferenceProject("cp1", ref)
 
     assert isinstance(module, ReferenceProject)
+
+def test_pack() -> None:
+    ref = ProjectReference("")
+    
+    expected = b'000E00180003673A2F00012E65BE02570017'
+    codepage = 0x04E4
+    codepage_name = "cp" + str(codepage)
+    module = ReferenceProject(codepage_name, ref)
+    results = module.pack(codepage_name, 'little')
+    assert results == expected
