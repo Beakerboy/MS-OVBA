b8061fd20c9a7f835b3fae8612d87f2f781becf3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-02-27 21:48:25 -0500

    Update reference_registered.py

--- src/ms_ovba/Models/Entities/reference_registered.py
+++ src/ms_ovba/Models/Entities/reference_registered.py
@@ -64,5 +64,5 @@ class ReferenceRegistered(ReferenceRecord):
             # raise a warning
             pass
 
-        libid_ref = LibidReference.create(libid_ref_bytes)
+        libid_ref = LibidReference.create(libid_ref_bytes, endien)
         return ReferenceRegistered("", libid_ref)
