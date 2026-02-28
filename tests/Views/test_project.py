a4c48021d5824bbee3f8ec812187b53bee8b9d2d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-02-28 17:36:31 -0500

    Update test_project.py

--- tests/Views/test_project.py
+++ tests/Views/test_project.py
@@ -24,19 +24,20 @@ class Obj:
 
 
 class Mod:
-    def __init__(self, name) -> None:
+    def __init__(self, name, type) -> None:
         self.modName = Obj(name)
         self.workspace = [0, 0, 0, 0, 'C']
+        self.type = type
 
     def to_project_module_string(self):
-        return "=" + self.modName.value
+        return self.type + "=" + self.modName.value
 
 
 class MockVbaProject:
     def __init__(self) -> None:
-        mod1 = Mod("Module1")
+        mod1 = Mod("Module1", "Module")
         mod1.workspace = [26, 26, 1349, 522, 'Z']
-        self.modules = [Mod("ThisWorkbook"), Mod("Sheet1"), mod1]
+        self.modules = [Mod("ThisWorkbook", "Document"), Mod("Sheet1", "Document"), mod1]
         self.project_id = '{9E394C0B-697E-4AEE-9FA6-446F51FB30DC}'
 
     def get_codepage_name(self):
