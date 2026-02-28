27b32436cdfe48c0c3877d0f8e1a172dcc24913c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-02-28 08:49:13 -0500

    Update test_vba_project.py

--- tests/Unit/Views/test_vba_project.py
+++ tests/Unit/Views/test_vba_project.py
@@ -29,7 +29,7 @@ def test_vba_project() -> None:
     vba_project = MockVbaProject()
     vba_project.performance_cache = b'\x00\x01\x02\x03'
     vba_project.performance_cache_version = 0x00B5
-    
+
     vba_project_view = ProjectView(vba_project)
     expected = b'\xCC\x61\xB5\x00\x00\x03\x00\x00\x01\x02\x03'
     assert vba_project_view.to_bytes() == expected
