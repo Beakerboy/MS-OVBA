0c831818d0db6aa78ae955f7015028caacd4231b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-02-27 22:06:27 -0500

    Update reference.py

--- src/ms_ovba/Models/Entities/reference.py
+++ src/ms_ovba/Models/Entities/reference.py
@@ -53,4 +53,4 @@ class Reference():
 
         bytestring = data[offset:]
         ref = ReferenceRecord.unpack(bytestring, endien)
-        return Record("cp", ref, name)
+        return Reference("cp", ref, name)
