0cd72ebdeb0fa3ee29f98a32f72a0d1e43bf1fcb
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-02-28 09:03:12 -0500

    Update test_projectWm.py

--- tests/Unit/Views/test_projectWm.py
+++ tests/Unit/Views/test_projectWm.py
@@ -14,7 +14,7 @@ class MockVbaProject:
 def test_project_wm() -> None:
     vba_project = MockVbaProject()
     project_wm = ProjectWm(vba_project)
-    
+
     expected = (b'ThisWorkbook\x00T\x00h\x00i\x00s\x00W\x00o\x00r\x00k\x00b'
                 + b'\x00o\x00o\x00k\x00\x00\x00Sheet1\x00S\x00h\x00e\x00e\x00'
                 + b't\x001\x00\x00\x00Module1\x00M\x00o\x00d\x00u\x00l\x00e'
