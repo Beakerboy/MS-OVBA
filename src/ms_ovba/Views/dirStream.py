4ae368e023392d8ffd8c8645d72c5dfc35d4e62b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-01 10:32:53 -0500

    Update dirStream.py

--- src/ms_ovba/Views/dirStream.py
+++ src/ms_ovba/Views/dirStream.py
@@ -6,7 +6,7 @@ from ms_ovba.Models.Fields.doubleEncodedString import (
     DoubleEncodedString
 )
 from ms_ovba.Models.Fields.packed_data import PackedData
-from typing import TypeVar
+from typing import List, TypeVar
 
 
 T = TypeVar('T', bound='DirStream')
@@ -19,32 +19,29 @@ class DirStream():
 
     def __init__(self: T, project: VbaProject) -> None:
         self.project = project
-        self.information = []
-        self.references = []
-        self.modules = []
         self._include_compat = False
 
     def to_bytes(self: T) -> bytes:
-        self._load_information()
+        information = self._load_information()
         endien = self.project.endien
         codepage_name = self.project.get_codepage_name()
         pack_symbol = '<' if endien == 'little' else '>'
         # should be 0xFFFF
         cookie_value = self.project.get_project_cookie()
         self.project_cookie = IdSizeField(19, 2, cookie_value)
-        self.references = self.project.references
-        self.modules = self.project.modules
+        references = self.project.references
+        modules = self.project.modules
         output = b''
-        for record in self.information:
+        for record in information:
             output += record.pack(codepage_name, endien)
-        for record in self.references:
+        for record in references:
             output += record.pack(codepage_name, endien)
 
         modules_header = IdSizeField(0x000F, 2, len(self.modules))
 
         output += (modules_header.pack(codepage_name, endien)
                    + self.project_cookie.pack(codepage_name, endien))
-        for record in self.modules:
+        for record in modules:
             output += record.pack(codepage_name, endien)
         output += struct.pack(pack_symbol + "HI", 16, 0)
         return output
@@ -59,14 +56,14 @@ class DirStream():
         bin_f.write(compressed)
         bin_f.close()
 
-    def _load_information(self: T) -> None:
-        self.codepage = 0x04E4
+    def _load_information(self: T) -> List:
+        codepage = 0x04E4
         # 0=16bit, 1=32bit, 2=mac, 3=64bit
         syskind = IdSizeField(1, 4, 3)
         compat_version = IdSizeField(74, 4, 3)
         lcid = IdSizeField(2, 4, 0x0409)
         lcid_invoke = IdSizeField(20, 4, 0x0409)
-        codepage_record = IdSizeField(3, 2, self.codepage)
+        codepage_record = IdSizeField(3, 2, codepage)
         project_name = IdSizeField(4, 10, "VBAProject")
         docstring = DoubleEncodedString([5, 0x0040], "")
         helpfile = DoubleEncodedString([6, 0x003D], "")
@@ -76,10 +73,10 @@ class DirStream():
         minor_version = PackedData("H", 17)
         constants = DoubleEncodedString([12, 0x003C], "")
 
-        self.information = [syskind]
+        information = [syskind]
         if self._include_compat:
-            self.information.append(compat_version)
-        self.information.extend([
+            information.append(compat_version)
+        information.extend([
             lcid,
             lcid_invoke,
             codepage_record,
