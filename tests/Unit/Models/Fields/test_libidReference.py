1690a88f891e24a1e87a8184e78577c6c3174068
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-02-28 22:57:32 -0500

    Update test_libidReference.py

--- tests/Unit/Models/Fields/test_libidReference.py
+++ tests/Unit/Models/Fields/test_libidReference.py
@@ -30,6 +30,7 @@ def test_posix() -> None:
                 "#2.0#0#//usr/bin/stdole2.tlb#OLE Automation")
     assert str(libid_ref) == expected
 
+
 def test_unpack() -> None:
     data = (b'*\\G{00020430-0000-0000-C000-000000000046}'
             b'#2.0#0#C:\\Windows\\System32\\stdole2.tlb#OLE Automation')
