b13cced918d458493608f3b79283cadc57681ff5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-02 10:45:06 -0500

    Update test_idSizeField.py

--- tests/Unit/Models/Fields/test_idSizeField.py
+++ tests/Unit/Models/Fields/test_idSizeField.py
@@ -6,7 +6,7 @@ def test_string() -> None:
     field = IdSizeField(1, 2, "Hi")
     expected = b'\x01\x00\x02\x00\x00\x00Hi'
     assert field.pack("cp", "little") == expected
-
+    
 
 def test_H():
     field = IdSizeField(1, 2, 3)
