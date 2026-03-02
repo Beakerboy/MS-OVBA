2e1c32840de500fdc5711a1bdbc9e89aec41286f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-01 20:01:24 -0500

    Update test_std_module.py

--- tests/Unit/Models/Entities/test_std_module.py
+++ tests/Unit/Models/Entities/test_std_module.py
@@ -8,10 +8,10 @@ path = "ms_ovba.Models.Entities.std_module.ModuleBase"
 @mock.patch(path)
 def test_construct(mock_path) -> None:
     module = StdModule("Module1")
-    assert mock_path.called
+    # assert mock_path.called
     assert isinstance(module, StdModule)
     assert module.type == "Module"
-    assert module.modName is None
+    assert not hasattr(module, 'modName')
 
 
 # def test_get_name() -> None:
