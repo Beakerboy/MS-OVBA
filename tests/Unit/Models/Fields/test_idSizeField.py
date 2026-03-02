c0d7246e75fbfe3d17b7c3a2be979ec0ab48c3f5
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-02 10:42:47 -0500

    Update test_idSizeField.py

--- tests/Unit/Models/Fields/test_idSizeField.py
+++ tests/Unit/Models/Fields/test_idSizeField.py
@@ -2,10 +2,18 @@ import pytest
 from ms_ovba.Models.Fields.idSizeField import IdSizeField
 
 
+def test_string() -> None:
+    field = IdSizeField(1, 2, "Hi")
+    expected = b'\x01\x00\x02\x00\x00\x00Hi'
+    assert field.pack("cp", "little") == expected
+
+
 def test_H():
     field = IdSizeField(1, 2, 3)
-    assert field.pack("cp", "little") == b'\x01\x00\x02\x00\x00\x00\x03\x00'
-
+    expected = b'\x01\x00\x02\x00\x00\x00\x03\x00'
+    assert field.pack("cp", "little") == expected
+    
+    
 
 def test_I():
     field = IdSizeField(1, 4, 3)
