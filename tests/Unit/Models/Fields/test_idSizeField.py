71fb97d8b8be580ab1c7774288598668db342b10
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-02 09:49:05 -0500

    Update test_idSizeField.py

--- tests/Unit/Models/Fields/test_idSizeField.py
+++ tests/Unit/Models/Fields/test_idSizeField.py
@@ -2,6 +2,10 @@ import pytest
 from ms_ovba.Models.Fields.idSizeField import IdSizeField
 
 
+def test_H():
+    field = IdSizeField(1, 2, 3)
+    assert field.pack == b'\x01\x00\x01\x00\x03\x00'
+
 def test_bad_value() -> None:
     field = IdSizeField(2, 3, 6)
     with pytest.raises(Exception):
