e584a387ab5f20aef54004dbdcf720de2f27746b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-01 19:29:09 -0500

    Update test_std_module.py

--- tests/Unit/Models/Entities/test_std_module.py
+++ tests/Unit/Models/Entities/test_std_module.py
@@ -8,7 +8,7 @@ def test_construct() -> None:
         module = StdModule("Module1")
         assert isinstance(module, StdModule)
         assert module.type == "Module"
-        assert module.modname is None
+        assert module.modName is None
 
 
 # def test_get_name() -> None:
