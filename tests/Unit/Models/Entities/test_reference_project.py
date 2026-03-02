adbf425ee71e7c144a4974555a3afa126fc9991e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-02 11:01:25 -0500

    Update test_reference_project.py

--- tests/Unit/Models/Entities/test_reference_project.py
+++ tests/Unit/Models/Entities/test_reference_project.py
@@ -38,7 +38,7 @@ def test_pack() -> None:
                     "BE 65 17 00")
     expected = bytes.fromhex(" ".join(expected_hex))
     codepage = 0x04E4
-    codepage_name = "cp" + str(codepage)
-    ref_proj = ReferenceProject(codepage_name, ref)
-    results = ref_proj.pack(codepage_name, 'little')
+    cp_name = "cp" + str(codepage)
+    ref_proj = ReferenceProject(ref)
+    results = ref_proj.pack('little', cp_name)
     assert results == expected
