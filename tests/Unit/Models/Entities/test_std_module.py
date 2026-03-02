55c24a4b7e8231e2e66bda9ea268f248546d5c2c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-01 19:04:51 -0500

    Update test_std_module.py

--- tests/Unit/Models/Entities/test_std_module.py
+++ tests/Unit/Models/Entities/test_std_module.py
@@ -3,15 +3,14 @@ from ms_ovba.Models.Entities.std_module import StdModule
 
 
 def test_construct() -> None:
-    cache = b'foo'
     path = "ms_ovba.Models.Entities.std_module.ModuleBase"
     with mock.patch(path) as MockSuper:
         module = StdModule("Module1")
         assert isinstance(module, StdModule)
         assert module.type == "Module"
-        assert module.modname == None
+        assert module.modname is None
 
 
-#def test_get_name() -> None:
+# def test_get_name() -> None:
 #    module = StdModule("Module1")
 #    assert module.get_name() == "Module1"
