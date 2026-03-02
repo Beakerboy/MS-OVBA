20b7ee54c5ec6711c9ffb996f4738c7d5dc0eca3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-02 11:34:56 -0500

    Update test_double_encoded_string.py

--- tests/Unit/Models/Fields/test_double_encoded_string.py
+++ tests/Unit/Models/Fields/test_double_encoded_string.py
@@ -20,4 +20,4 @@ def test_pack() -> None:
     de_string = DoubleEncodedString([0x01, 0x02], "Foo")
     expected = (b'\x01\x00\x03\x00\x00\x00Foo' +
                 b'\x02\x00\x06\x00\x00\x00F\x00o\x00o\x00')
-    assert de_string.pack("little",cp_name) == expected
+    assert de_string.pack("little", cp_name) == expected
