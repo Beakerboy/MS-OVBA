931c97dc10ca7e57c330458d05ed058ff66446df
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-01 20:35:53 -0500

    Rename test_doc_module.py to test_doc_module.py (#123)
    * standard module is now an honest UnitTest

--- tests/Unit/Models/Entities/test_std_module.py
+++ tests/Unit/Models/Entities/test_std_module.py
@@ -1,13 +1,13 @@
+from unittest import mock
 from ms_ovba.Models.Entities.std_module import StdModule
 
 
-def test_set_get_cache() -> None:
-    module = StdModule("Module1")
-    cache = b'foo'
-    module.set_cache(cache)
-    assert module.get_cache() == cache
+path = "ms_ovba.Models.Entities.std_module.ModuleBase.__init__"
 
 
-def test_get_name() -> None:
+@mock.patch(path, return_value=None)
+def test_construct(mock_base_init) -> None:
     module = StdModule("Module1")
-    assert module.get_name() == "Module1"
+    mock_base_init.assert_called_once_with("Module1")
+    assert isinstance(module, StdModule)
+    assert module.type == "Module"
