d7a15d7d41f366a1139f49562f1b9eb7d5851dcf
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-02 10:53:19 -0500

    Update idSizeField.py

--- src/ms_ovba/Models/Fields/idSizeField.py
+++ src/ms_ovba/Models/Fields/idSizeField.py
@@ -16,7 +16,7 @@ class IdSizeField():
         self._size = size
         self._value = value
 
-    def pack(self: T, codepage_name: str, endien: str) -> bytes:
+    def pack(self: T, endien: str) -> bytes:
         endien_symbol = '<' if endien == 'little' else '>'
         format = endien_symbol + "HI"
         if isinstance(self._value, str):
