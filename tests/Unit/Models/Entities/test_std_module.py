9a40e7858f288077362f2f263efbea5b2c6c406f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-01 20:32:49 -0500

    Update test_std_module.py

--- tests/Unit/Models/Entities/test_std_module.py
+++ tests/Unit/Models/Entities/test_std_module.py
@@ -5,7 +5,7 @@ from ms_ovba.Models.Entities.std_module import StdModule
 path = "ms_ovba.Models.Entities.std_module.ModuleBase.__init__"
 
 
-@patch(path, return_value=None)
+@mock.patch(path, return_value=None)
 def test_construct(mock_base_init) -> None:
     module = StdModule("Module1")
     mock_base_init.assert_called_once_with("Module1")
