bacc232c4af8c05c614863478c3de14c42d7692d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-03 17:27:22 -0500

    Update test_fullFile.py

--- tests/Functional/test_fullFile.py
+++ tests/Functional/test_fullFile.py
@@ -273,7 +273,11 @@ def create_cache(proj_cookie: int) -> bytes:
 
     record += neg_one_4b + struct.pack("<I", 0x30)
     ca += struct.pack("<I", len(record)) + record
-    names = ["Excel, "VBA", "Win16", "Win32", "Win64", "Mac", "VBA6", "VBA7"]
+    names = [
+        "Excel, "VBA", "Win16", "Win32", "Win64", "Mac", "VBA6", "VBA7",
+        "Project1", "stdole", "VBAProject", "Office", "ThisWorkbook",
+        "Evaluate", "Sheet1", "Module1", "Workbook"
+        ]
     for name in names:
         ca += struct.pack("<HI" + len(name) + "s", 0x2ba0, 0x04050000, name)
     return ca
