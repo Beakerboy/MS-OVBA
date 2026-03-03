92a613f0ac2ad60d82d90f0bf0692c91960bc17e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-03 10:29:07 -0500

    Update test_fullFile.py

--- tests/Functional/test_fullFile.py
+++ tests/Functional/test_fullFile.py
@@ -142,7 +142,7 @@ def test_full_file() -> None:
                 b'\x00o\x00k\x00\x00\x00Sheet1\x00S\x00' +
                 b'h\x00e\x00e\x00t\x001\x00\x00\x00Modu' +
                 b'le1\x00M\x00o\x00d\x00u\x00l\x00e\x00' +
-                b'1\x00')
+                b'1\x00\x00\x00\x00\x00')
     assert projectwm.to_bytes() == expected
 
     # Check Project
