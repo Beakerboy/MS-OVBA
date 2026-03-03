aaeb230f5fe3608f871fade55f9b8fcaa012fe2f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-03 16:20:15 -0500

    Update test_fullFile.py

--- tests/Functional/test_fullFile.py
+++ tests/Functional/test_fullFile.py
@@ -259,9 +259,10 @@ def create_cache(proj_cookie: int) -> bytes:
 
     ca += struct.pack("<IH", 0xFFFFFFFF, 0x0101)
     neg_one_4b = b'\xFF\xFF\xFF\xFF'
+    neg_one_one = neg_one_4b + struct.pack("<I", 1)
     record = (neg_one_4b * 13 + struct.pack("<2I", 0x0230, 0x0218) +
               neg_one_4b * 28 + struct.pack("<I", 0x0200) + neg_one_4b * 84 +
-              b'\xf1q\x9a\xee\xc0\xe0\xc4F\xa2\xf8l|\xf9{s')
+              b'\xf1q\x9a\xee\xc0\xe0\xc4F\xa2\xf8l|\xf9{s'+ neg_one_one)
     ca += struct.pack("<I", len(record)) + record
     return ca
 
