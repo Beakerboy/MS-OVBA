0a7feb9d608eada5c4ffb5219b2455b70186fba6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-11 14:30:06 -0400

    Update test_dirObject.py

--- tests/Functional/test_dirObject.py
+++ tests/Functional/test_dirObject.py
@@ -99,6 +99,10 @@ def test_dirstream() -> None:
     project.add_module(sheet1)
     project.add_module(module1)
 
+    f.seek(0x800)
+    expected_cache = f.read(0x333)
+    assert sheet1.cache == expected_cache
+
     assert stream.to_bytes() == decompressed_stream
 
     # The OEM and 3rd party compression results are not the same,
