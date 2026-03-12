200f08d7cbc92799b9e943d2c52cafc855d250e6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-12 09:14:36 -0400

    Update test_reference_registered.py

--- tests/Unit/Models/Entities/test_reference_registered.py
+++ tests/Unit/Models/Entities/test_reference_registered.py
@@ -57,3 +57,18 @@ def test_unpack():
                                   b'000000000046}#2.0#0#' +
                                   b'C:\\Windows\\system32\\stdole2.tlb#' +
                                   b'OLE Automation')
+
+def test_bad_id():
+    hex = ("0C 00 68 00 00 00 5E 00 00 00 2A 5C 47 7B 30 30",
+           "30 32 30 34 33 30 2D 30 30 30 30 2D 30 30 30 30",
+           "2D 43 30 30 30 2D 30 30 30 30 30 30 30 30 30 30",
+           "34 36 7D 23 32 2E 30 23 30 23 43 3A 5C 57 69 6E",
+           "64 6F 77 73 5C 73 79 73 74 65 6D 33 32 5C 73 74",
+           "64 6F 6C 65 32 2E 74 6C 62 23 4F 4C 45 20 41 75",
+           "74 6F 6D 61 74 69 6F 6E 00 00 00 00 00 00")
+    data = bytes.fromhex(" ".join(hex))
+    path = 'ms_ovba.Models.Entities.reference_registered.LibidReference'
+    with mock.patch(path, MockLibid2):
+        with pytest.raises(Exception):
+            ref = ReferenceRegistered.unpack(data, "little")
+        
