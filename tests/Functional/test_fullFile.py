96b26ed492dd386a44722a64c7b2c3c562a79795
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-12 19:51:26 -0400

    Update test_fullFile.py

--- tests/Functional/test_fullFile.py
+++ tests/Functional/test_fullFile.py
@@ -148,6 +148,7 @@ def test_full_file() -> None:
     project.add_attribute("HelpContextID", "0")
     project.add_attribute("VersionCompatible32", "393222000")
     project.include_compat()
+    project.include_projectwm()
 
     # Check _VBA_Project
     pv_bytes = ProjectView(project).to_bytes()
