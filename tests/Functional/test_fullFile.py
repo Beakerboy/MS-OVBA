1b0ad730f646ce0bd6d100375fff505d699d3f8d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-12 10:22:07 -0400

    Update test_fullFile.py

--- tests/Functional/test_fullFile.py
+++ tests/Functional/test_fullFile.py
@@ -177,7 +177,7 @@ def test_full_file() -> None:
 
     # Check Modules
 
-    path = bin_path
+    path = "Module1.bin"
     cache_size = 0x333
     bin_path = "vbaProject.bin"
     bin_offset = 0x1200
