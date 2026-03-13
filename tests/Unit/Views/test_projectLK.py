6f82ab0f1b7263d8f4438f019b7bb38c0dab4fb1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-13 11:12:45 -0400

    Create test_projectLK.py

---
+++ tests/Unit/Views/test_projectLK.py
@@ -0,0 +1,16 @@
+from ms_ovba.Views.project_lk import ProjectLk
+
+
+class MockVbaProject:
+    def __init__(self) -> None:
+        self.licenses = []
+
+
+def test_project_lk() -> None:
+    vba_project = MockVbaProject()
+    project_lk = ProjectLk(vba_project)
+
+    expected = (b'\x01\x00\x00\x00\x00\x00')
+    result = project_lk.to_bytes()
+    assert len(result) == 86
+    assert result == expected
