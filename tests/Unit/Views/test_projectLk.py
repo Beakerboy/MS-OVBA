d5b1da4116fc4753128bcb0f737fe17b332bb3ec
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-13 14:30:34 -0400

    Update test_projectLk.py

--- tests/Unit/Views/test_projectLk.py
+++ tests/Unit/Views/test_projectLk.py
@@ -5,11 +5,25 @@ class MockVbaProject:
     def __init__(self) -> None:
         self._license_records = []
 
+class MockLicense:
+    def to_bytes() -> bytes:
+        return b'Test'
 
-def test_project_lk() -> None:
+def test_project_lk_empty() -> None:
     vba_project = MockVbaProject()
     project_lk = ProjectLk(vba_project)
 
     expected = (b'\x01\x00\x00\x00\x00\x00')
+    result = project_lk.to_bytes()
+    assert result == expected
+
+
+def test_project_lk() -> None:
+    vba_project = MockVbaProject()
+    mock_license = MockLicense()
+    vba_project._license_records = [mock]
+    project_lk = ProjectLk(vba_project)
+
+    expected = (b'\x01\x00\x01\x00\x00\x00Test')
     result = project_lk.to_bytes()
     assert result == expected
