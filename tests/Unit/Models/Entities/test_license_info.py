fa390979796f935d3c673d737183a8f261d982a7
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-13 17:14:13 -0400

    Update test_license_info.py

--- tests/Unit/Models/Entities/test_license_info.py
+++ tests/Unit/Models/Entities/test_license_info.py
@@ -6,6 +6,6 @@ def test_to_bytes() -> None:
     guid = uuid.UUID("2DF8D04C5BFA101BBDE500AA0044DE52")
     key = b'Key'
     out = LicenseInfo(guid, key)
-    expected = (b'\x2D\xF8\xD0\x4C\x5B\xFA\x10\x1B\xBD\xE5\x00\xAA\x00\x44\xDE\x52' +
+    expected = (b'-\xF8\xD0L[\xFA\x10\x1B\xBD\xE5\x00\xAA\x00D\xDER' +
                 b'\x04\x00\x00\x00Key\x04\x00\x00\x00')
     assert out.to_bytes() == expected
