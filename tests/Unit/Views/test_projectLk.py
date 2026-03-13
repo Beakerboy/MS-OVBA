7d644c1f269442b738a9cb27755cd583454cd54f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-13 15:46:44 -0400

    Update test_projectLk.py

--- tests/Unit/Views/test_projectLk.py
+++ tests/Unit/Views/test_projectLk.py
@@ -5,9 +5,11 @@ class MockVbaProject:
     def __init__(self) -> None:
         self._license_records = []
 
+
 class MockLicense:
     def to_bytes() -> bytes:
         return b'Test'
+
 
 def test_project_lk_empty() -> None:
     vba_project = MockVbaProject()
@@ -21,7 +23,7 @@ def test_project_lk_empty() -> None:
 def test_project_lk() -> None:
     vba_project = MockVbaProject()
     mock_license = MockLicense()
-    vba_project._license_records = [mock]
+    vba_project._license_records = [mock_license]
     project_lk = ProjectLk(vba_project)
 
     expected = (b'\x01\x00\x01\x00\x00\x00Test')
