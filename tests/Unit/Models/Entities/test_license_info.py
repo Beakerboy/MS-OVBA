a11252917c21bccf529ed48c26f8062f0a13a7e2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-13 16:59:27 -0400

    Update test_license_info.py

--- tests/Unit/Models/Entities/test_license_info.py
+++ tests/Unit/Models/Entities/test_license_info.py
@@ -3,9 +3,9 @@ from ms_ovba.Models.Entities.license_info import LicenseInfo
 
 
 def test_to_bytes() -> None:
-  guid = uuid.UUID("2DF8D04C5BFA101BBDE500AA0044DE52")
-  key = b'Key'
-  out = LicenseInfo(guid, key)
-  expected = (b'\x2D\xF8\xD0\x4C\x5B\xFA\x10\x1B\xBD\xE5\x00\xAA\x00\x44\xDE\x52' +
-              b'\x04\x00\x00\x00Key\x04\x00\x00\x00')
-  assert out.to_bytes() == expected
+    guid = uuid.UUID("2DF8D04C5BFA101BBDE500AA0044DE52")
+    key = b'Key'
+    out = LicenseInfo(guid, key)
+    expected = (b'\x2D\xF8\xD0\x4C\x5B\xFA\x10\x1B\xBD\xE5\x00\xAA\x00\x44\xDE\x52' +
+                b'\x04\x00\x00\x00Key\x04\x00\x00\x00')
+    assert out.to_bytes() == expected
