bc9ff5e58e0a40cc785bc20730f17c26166abc82
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-16 13:47:19 -0400

    Create test_reference_control.py

---
+++ tests/Unit/Models/Entities/test_reference_control.py
@@ -0,0 +1,56 @@
+import uuid
+from ms_ovba.Models.Entities.reference_control import ReferenceControl
+from unittest import mock
+
+
+class MockDEString:
+    def __init__(self, foo, bar) -> None:
+        pass
+
+    def pack(self, one, two) -> bytes:
+        return (b'\x16\x00\x0B\x00\x00\x00VBAProject1' +
+                b'\x3E\x00\x16\x00\x00\x00V\x00B\x00A\x00P\x00r' +
+                b'\x00o\x00j\x00e\x00c\x00t\x001\x00')
+
+
+class MockRefProj:
+    def pack(self, foo, bar) -> bytes:
+        return (b'\x0e\x00^\x00\x00\x000\x00\x00\x00' +
+                b'*\\CC:\\Example Path\\Example-ReferencedProject.xls ' +
+                b'\x00\x00\x00*\\CExample-ReferencedProject.xls' +
+                b'W\x02\xbee\x17\x00')
+
+
+def test_constructor1() -> None:
+    guid = uiid.UUID()
+    ref_proj = ""
+    ref = ReferenceControl(ref_proj)
+    assert isinstance(ref, ReferenceControl)
+
+
+def test_constructor2() -> None:
+    ref = ReferenceControl("", "VBAProject1")
+    assert isinstance(ref, ReferenceControl)
+
+
+def test_pack() -> None:
+    ref_proj = MockRefProj()
+    ref = Reference(ref_proj, "VBAProject1")
+
+    expected_hex = ("16 00 0B 00 00 00 56 42 41 50 72 6F 6A 65 63 74",
+                    "31 3E 00 16 00 00 00 56 00 42 00 41 00 50 00 72",
+                    "00 6F 00 6A 00 65 00 63 00 74 00 31 00 0E 00 5E",
+                    "00 00 00 30 00 00 00 2A 5C 43 43 3A 5C 45 78 61",
+                    "6D 70 6C 65 20 50 61 74 68 5C 45 78 61 6D 70 6C",
+                    "65 2D 52 65 66 65 72 65 6E 63 65 64 50 72 6F 6A",
+                    "65 63 74 2E 78 6C 73 20 00 00 00 2A 5C 43 45 78",
+                    "61 6D 70 6C 65 2D 52 65 66 65 72 65 6E 63 65 64",
+                    "50 72 6F 6A 65 63 74 2E 78 6C 73 57 02 BE 65 17",
+                    "00")
+    expected = bytes.fromhex(" ".join(expected_hex))
+    codepage = 0x04E4
+    cp_name = "cp" + str(codepage)
+    path = 'ms_ovba.Models.Entities.reference.DoubleEncodedString'
+    with mock.patch(path, MockDEString):
+        results = ref.pack('little', cp_name)
+        assert results == expected
