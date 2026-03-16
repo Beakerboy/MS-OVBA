baa07da0f960b70686e1443f2df403c67160818c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-16 09:38:40 -0400

    Update reference_control.py

--- src/ms_ovba/Models/Entities/reference_control.py
+++ src/ms_ovba/Models/Entities/reference_control.py
@@ -1,4 +1,5 @@
 import struct
+import uuid
 from ms_ovba.Models.Entities.reference_record import ReferenceRecord
 from ms_ovba.Models.Fields.project_reference import ProjectReference
 from typing import TypeVar
@@ -11,19 +12,35 @@ class ReferenceControl(ReferenceRecord):
     """
     2.3.4.2.2.3 REFERENCECONTROL Record
     """
-    def __init__(self: T, ref: ProjectReference, name: str = None) -> None:
+    def __init__(self: T, ref: ProjectReference,
+                 ref2: ProjectReference,
+                 name: str = None
+                 guid uuid.UUID, cookie: int) -> None:
         self._libid_twiddled = ref
+        self._libid_extended = ref2
         name_record_extended = name
 
     def pack(self: T, endien: str, cp_name: str) -> bytes:
         ref_str = str(self._libid_twiddled).encode(cp_name)
         size_of_libid_twiddled = len(ref_str)
         size_twiddled = 10 + size_of_libid_twiddled
+        ref2_str = str(self._libid_extended).encode(cp_name)
+        size_of_libid_extended = len(ref2_str)
+        size_extended = 30 + size_of_libid_extended
         endien_symbol = '<' if endien == 'little' else '>'
         format = (
             endien_symbol + "HII" + str(size_of_libid_twiddled) +
             "sIH" + str(libid_rel_size) + "sIH")
-        
-        return struct.pack(
-            format, 0x2F, size_twiddled, size_of_libid_twiddled
-                           ref_str, 0, 0, ref_str_rel, 0x30, 0x0017)
+        if self._refname is not None:
+            name_de = DoubleEncodedString([0x0016, 0x003E], self._refname)
+            name_pack = name_de.pack(endien, cp_name)
+
+        return (
+            struct.pack(
+                format, 0x2F, size_twiddled, size_of_libid_twiddled
+                ref_str, 0, 0
+            ) + name_pack +
+            struct.pack(
+                endien_symbol + "HII", 0x30, size_extended, size_of_libid_extended) +
+            ref2_str +
+            struct.pack(endien_symbol + "IH16sI", 0, 0, guid.bytes, cookie)
