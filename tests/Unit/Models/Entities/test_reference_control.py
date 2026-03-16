d6013123fce07dffa17aadfc8c35f93cd9b67b9d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-16 15:37:51 -0400

    Update test_reference_control.py

--- tests/Unit/Models/Entities/test_reference_control.py
+++ tests/Unit/Models/Entities/test_reference_control.py
@@ -15,8 +15,8 @@ class MockDEString:
 
 class MockLibIdExt:
     def __str__(self) -> str:
-        return ("*\G{896C2D83-5466-46ED-8FAE-4C3E4F85E710}#2.0#" +
-            "0#C:\Users\jsmith\AppData\Local\Temp\VBE\MSForms.exd#" +
+        return ("*\\G{896C2D83-5466-46ED-8FAE-4C3E4F85E710}#2.0#" +
+            "0#C:\\Users\\jsmith\\AppData\\Local\\Temp\\VBE\\MSForms.exd#" +
             "Microsoft Forms 2.0 Object Library")
 
 
