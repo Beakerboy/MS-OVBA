31b19dc1a53bd91a9d63e7408ce52d19a8bb9381
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-19 19:28:06 -0400

    Update test_libidReference.py

--- tests/Unit/Models/Fields/test_libidReference.py
+++ tests/Unit/Models/Fields/test_libidReference.py
@@ -33,10 +33,10 @@ def test_posix() -> None:
 
 
 @pytest.mark.parametrize(
-    "data",[
+    "data", [
         (b'*\\G{00020430-0000-0000-C000-000000000046}' +
-     b'#2.0#0#C:\\Windows\\System32\\stdole2.tlb#OLE Automation'),
-    (b"*\\G{00000000-0000-0000-0000-000000000000}#0.0#0##")
+         b'#2.0#0#C:\\Windows\\System32\\stdole2.tlb#OLE Automation'),
+        (b"*\\G{00000000-0000-0000-0000-000000000000}#0.0#0##")
     ])
 def test_unpack(data: bytes) -> None:
     lib = LibidReference.unpack(data)
