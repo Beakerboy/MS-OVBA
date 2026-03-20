8290a139b047443620963d93c154b9c16b7eff90
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-20 13:22:55 -0400

    Create test_reference_original.py

---
+++ tests/Unit/Models/Entities/test_reference_original.py
@@ -0,0 +1,54 @@
+import pytest
+from unittest import mock
+from ms_ovba.Models.Entities.reference_original import ReferenceOriginal
+
+
+class MockLibid:
+    def __len__(self) -> int:
+        return 8
+
+    def __str__(self) -> str:
+        return ("Test#Lib")
+
+
+class MockRefCntl:
+    def pack(self) -> bytes:
+        return b'TestRef'
+    def unpack(self, endien):
+        return MockRefCntl()
+
+
+def test_constructor() -> None:
+    lib = MockLibid()
+    ref = MockRefCntl()
+    module = ReferenceOriginal(lib, ref)
+    assert isinstance(module, ReferenceRegistered)
+
+
+def test_pack() -> None:
+    expected = b'\x33\x00\x08\x00\x00\x00Test#LibTestRef'
+    codepage = 0x04E4
+    cp_name = "cp" + str(codepage)
+    ref_reg = ReferenceOriginal(MockLibid1(), MockRefCntl())
+    results = ref_reg.pack('little', cp_name)
+    assert results == expected
+
+
+def test_bad_id():
+    hex = (
+        b'\x33\x00\x32\x00\x00\x00'
+        br'*\G{00000000-0000-0000-0000-000000000000}#0.0#0##'
+        b'/\x00;\x00\x00\x001\x00\x00\x00'
+        br'*\G{00000000-0000-0000-0000-000000000000}#0.0#0##'
+        b'\x00\x00\x00\x00\x00\x00'
+        br'*\G{00000000-0000-0000-0000-000000000000}#0.0#0##'
+        b'\x00\x00\x00\x00\x00\x00'
+        b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+        b'\x01\x00\x00\x00'
+    )
+    path = 'ms_ovba.Models.Entities.reference_registered.LibidReference'
+    with mock.patch(path, MockLibid):
+        path1 = 'ms_ovba.Models.Entities.reference_registered.ReferenceControl'
+        with mock.patch(path, MockLibid):
+            with pytest.raises(Exception):
+                ReferenceRegistered.unpack(data, "little")
