fa009704a8c61a86f038a9ddf54be86de012e091
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-20 10:42:40 -0400

    Update test_libidReference.py

--- tests/Unit/Models/Fields/test_libidReference.py
+++ tests/Unit/Models/Fields/test_libidReference.py
@@ -21,7 +21,7 @@ def test_str(guid, ver, lib, path, name, expected) -> None:
     assert str(libid_ref) == expected
 
 
-def test_str_posix(guid, ver, lib, path, name, expected) -> None:
+def test_str_posix() -> None:
     guid = uuid.UUID('00000000000000000000000000000000')
     libid_ref = LibidReference(guid, "2.0", "0", "", "", False)
     expected = r"*\H{00000000-0000-0000-0000-000000000000}#2.0#0##"
