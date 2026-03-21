32721f6d1ded603604fce405656c8b8eb908b2d8
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-21 15:49:07 -0400

    Update test_vba_project.py

--- tests/Unit/Views/test_vba_project.py
+++ tests/Unit/Views/test_vba_project.py
@@ -3,14 +3,13 @@ from unittest import mock
 
 
 mock_vbaproject = mock.MagicMock()
-pcv = mock.PropertyMock(return_value=0xb5)
-pc = mock.PropertyMock(return_value=b'\x00\x01\x02\x03')
-type(mock_vbaproject).performance_cache_version = pcv
-type(mock_vbaproject).performance_cache = pc
 
 
 def test_vba_project_default() -> None:
-    vba_project = MockVbaProject()
+    pcv = mock.PropertyMock(return_value=0xFFFF)
+    pc = mock.PropertyMock(return_value=b'')
+    type(mock_vbaproject).performance_cache_version = pcv
+    type(mock_vbaproject).performance_cache = pc
     vba_project_view = ProjectView(vba_project)
     expected = b'\xCC\x61\xFF\xFF\x00\x03\x00'
     assert vba_project_view.to_bytes() == expected
@@ -20,6 +19,10 @@ def test_vba_project() -> None:
     """
     Demonstrate the effect of performance cache on the project
     """
+    pcv = mock.PropertyMock(return_value=0xb5)
+    pc = mock.PropertyMock(return_value=b'\x00\x01\x02\x03')
+    type(mock_vbaproject).performance_cache_version = pcv
+    type(mock_vbaproject).performance_cache = pc
     vba_project_view = ProjectView(mock_vbaproject)
     expected = b'\xCC\x61\xB5\x00\x00\x03\x00\x00\x01\x02\x03'
     assert vba_project_view.to_bytes() == expected
