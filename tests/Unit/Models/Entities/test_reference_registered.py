45a79d110372973de40c285882e627a24847d7a6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-21 19:10:07 -0400

    Update test_reference_registered.py

--- tests/Unit/Models/Entities/test_reference_registered.py
+++ tests/Unit/Models/Entities/test_reference_registered.py
@@ -13,7 +13,7 @@ mock_libid.__str__.return_value = (
 
 class MockLibid2:
     @staticmethod
-    def unpack(data):
+    def unpack(data: bytes) -> T
         lib = MockLibid2()
         lib.data = data
         return lib
