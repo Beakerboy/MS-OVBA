c483752107e3beb3c28c99255851698bede16899
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-22 13:48:39 -0400

    Update reference_record.py

--- src/ms_ovba/Models/Entities/reference_record.py
+++ src/ms_ovba/Models/Entities/reference_record.py
@@ -15,7 +15,9 @@ class ReferenceRecord:
         from ms_ovba.Models.Entities.reference_registered import (
             ReferenceRegistered
         )
-        from ms_ovba.Models.Entities.reference_original import ReferenceOriginal
+        from ms_ovba.Models.Entities.reference_original import (
+            ReferenceOriginal
+        )
         endien_symbol = '<' if endien == 'little' else '>'
         id = struct.unpack(endien_symbol + "H", bytestring)
         if id == 0x000D:
