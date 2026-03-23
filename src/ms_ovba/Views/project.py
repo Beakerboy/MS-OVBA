e1d625fa41d1bb9fd1f0d4c9fe919fc9463997cc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-23 10:48:48 -0400

    Update project.py

--- src/ms_ovba/Views/project.py
+++ src/ms_ovba/Views/project.py
@@ -1,5 +1,5 @@
 import binascii
-import ms_ovba_crypto.ms_ovba_crypto.encrypt as encrypt
+import ms_ovba_crypto.ms_ovba_crypto
 from ms_ovba.vbaProject import VbaProject
 from typing import TypeVar
 
