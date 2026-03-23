1beea818e3a807d95cbd7e82f72913926b265e4b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-23 15:30:08 -0400

    Update test_vbaProject.py

--- tests/Unit/test_vbaProject.py
+++ tests/Unit/test_vbaProject.py
@@ -2,59 +2,53 @@ import pytest
 from ms_ovba.vbaProject import VbaProject
 
 
+project = VbaProject()
+
+
 def test_set_get_default_date() -> None:
-    project = VbaProject()
     date = project.default_date
     project.default_date = date
     assert project.default_date == date
 
 
 def test_set_get_project_id() -> None:
-    project = VbaProject()
     project.project_id = '{test}'
     assert project.project_id == '{test}'
 
 
 def test_set_get_visibility() -> None:
-    project = VbaProject()
     project.make_visible()
     assert project.visibility_state == b'\xff'
 
 
 def test_set_get_invisibility() -> None:
-    project = VbaProject()
     project.make_invisible()
     assert project.visibility_state == b'\x00'
 
 
 def test_get_protection() -> None:
-    project = VbaProject()
     assert project.protection_state == b'\00' * 4
 
 
 def test_set_user_protection() -> None:
-    project = VbaProject()
     project.user_protect()
     expected = b'\x80\x00\x00\x00'
     assert project.protection_state == expected
 
 
 def test_set_host_protection() -> None:
-    project = VbaProject()
     project.host_protect()
     expected = b'\x40\x00\x00\x00'
     assert project.protection_state == expected
 
 
 def test_set_vbe_protection() -> None:
-    project = VbaProject()
     project.vbe_protect()
     expected = b'\x20\x00\x00\x00'
     assert project.protection_state == expected
 
 
 def test_unset_protection() -> None:
-    project = VbaProject()
     project.user_protect()
     project.host_protect()
     project.vbe_protect()
@@ -68,51 +62,42 @@ def test_unset_protection() -> None:
 
 
 def test_set_get_password() -> None:
-    project = VbaProject()
     project.password = 0
     assert project.password == 0
 
 
 def test_set_get_cache() -> None:
-    project = VbaProject()
     project.performance_cache = b'0'
     assert project.performance_cache == b'0'
 
 
 def test_set_get_cache_version() -> None:
-    project = VbaProject()
     project.performance_cache_version = 0x01
     assert project.performance_cache_version == 1
 
 
 def test_set_get_codepage() -> None:
-    project = VbaProject()
     project.codepage_name = 'cp1253'
     assert project.codepage_name == 'cp1253'
 
 
 def test_set_get_cookie() -> None:
-    project = VbaProject()
     project.project_cookie = 0x02
     assert project.project_cookie == 2
 
 
 def test_include_projectwm() -> None:
-    project = VbaProject()
     project.include_projectwm()
     assert project.projectwm
 
 
 def test_exclude_projectwm() -> None:
-    project = VbaProject()
     project.exclude_projectwm()
     assert not project.projectwm
 
 
-def test_bad_visibility() -> None:
-    """
-    Visibility must be zero or 0xFF
-    """
-    project = VbaProject()
-    with pytest.raises(Exception):
-        project.visibility_state = 1
+def test_compat() -> None:
+    project.include_compat()
+    assert project._compat == True
+    project.exclude_compat()
+    assert project._compat == False
