17e6bc91b8fc35a285e9cfbd12e4be57d4d7a05b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-22 22:33:41 -0400

    Update vbaProject.py

--- src/ms_ovba/vbaProject.py
+++ src/ms_ovba/vbaProject.py
@@ -30,7 +30,7 @@ class VbaProject:
         self._license_records: list[LicenseInfo] = []
 
         # Attributes and values
-        self.attributes dict[str, str] = {}
+        self.attributes: dict[str, str] = {}
 
         self._project_cookie = 0xFFFF
 
