6445b3162ade57b5c9e4fe1a5ef2643ee3ca413e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-23 18:42:56 -0400

    Create password_hash.py

---
+++ src/ms_ovba/Models/Entities/password_hash.py
@@ -0,0 +1,17 @@
+from typing inport TypeVar
+
+
+T = TypeVar('T', bound='PasswordHash)')
+
+
+class PasswordHash:
+    def __init__(self, password: str) -> None:
+        self._password = password
+
+    def to_bytes(self: T) -> bytes:
+        grbit = struct.pack(">I", GrbitKey | GrbitHashNull)[1:]
+        key_no_nulls = self._remove_nulls(self._key)
+        output = b'\xff' + grbit
+
+    def _remove_nulls(self: T) -> bytes:
+        pass
