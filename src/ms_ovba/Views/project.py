df71999c126ffb9b415dc5ea6796c74f4bafe1d3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-30 13:52:50 -0400

    Update project.py

--- src/ms_ovba/Views/project.py
+++ src/ms_ovba/Views/project.py
@@ -34,6 +34,7 @@ class Project:
         for module in modules:
             result += [module.to_project_module_string()]
         result += ['Name="VBAProject"']
+        result += ['HelpContextID="0"']
         for name, value in self.attributes.items():
             result += [f'{name}="{value}"']
         cmg = MsOvbaCrypto.encrypt(project_id, project.protection_state)
