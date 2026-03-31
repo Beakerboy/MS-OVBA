87c8de7b69d9142b388af2a662e4c63158612810
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-31 15:04:50 -0400

    Update test_project.py

--- tests/Unit/Views/test_project.py
+++ tests/Unit/Views/test_project.py
@@ -90,6 +90,11 @@ def test_project_line_valid() -> None:
     assert Project._valid_project_id_line(line)
 
 
+def test_project_line_invalid() -> None:
+    line = 'ID={9E394C0B-697E-4AEE-9FA6-446F51FB30DC}'
+    assert not Project._valid_project_id_line(line)
+
+
 def test_valid() -> None:
     path = 'tests/blank/PROJECT'
     assert Project.is_valid(path)
