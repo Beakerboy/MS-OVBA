af0ddd74e0f89a75f668f637743f968c90291274
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-31 14:12:48 -0400

    Update project.py

--- src/ms_ovba/Views/project.py
+++ src/ms_ovba/Views/project.py
@@ -78,9 +78,11 @@ class Project:
         """
 
         with open(filename, 'r') as file:
+            i = 1
             for line in file:
                 if line[-2:] not in ["\r\n", "\n\r"]:
-                    return 1
+                    return i
+                i += 1
         with open(filename, 'r') as file:
             line = file.readline().strip()
             if not Project._valid_project_id_line(line):
