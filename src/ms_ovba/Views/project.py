b995774e6b5d4f8180eee61aa1d60879975e392e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-31 21:40:36 -0400

    Update project.py

--- src/ms_ovba/Views/project.py
+++ src/ms_ovba/Views/project.py
@@ -239,7 +239,7 @@ class Project:
         if pieces[0] == "DPB":
             return Project._valid_quoted_hex(pieces[1], 16, 16)
         return False
-        
+
     # Data Type Validators
     @staticmethod
     def _valid_hex32(hex: str) -> bool:
