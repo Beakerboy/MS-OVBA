7c9f8857d7058283dd94539bd46e6e10c3688a07
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-01 13:12:00 -0400

    Update test_project.py

--- tests/Unit/Views/test_project.py
+++ tests/Unit/Views/test_project.py
@@ -113,3 +113,18 @@ def test_valid() -> None:
 def test_incorrect_line_endings() -> None:
     path = 'tests/test_files/PROJECT_bad'
     assert not Project.is_valid(path)
+
+
+file_numbers = [
+    ("1"),
+    ("2"),
+    ("3"),
+    ("4"),
+    ("5"),
+]
+
+
+@pytest.mark.parametrize("input, expected", file_numbers)
+def test_incorrect_lines(number: str) -> None:
+    path = 'tests/test_files/PROJECT_line' + number
+    assert not Project.is_valid(path)
