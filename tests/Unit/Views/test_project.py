41286561c73486baece2eccc6248ccb118fc2e5f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-02 12:43:59 -0400

    Update test_project.py

--- tests/Unit/Views/test_project.py
+++ tests/Unit/Views/test_project.py
@@ -179,7 +179,7 @@ file_numbers = [
 @pytest.mark.parametrize("number", file_numbers)
 def test_incorrect_lines(number: str) -> None:
     path = 'tests/test_files/PROJECT_line' + number
-    msg = f"Incorrect Line ending: {path} line: {number}"
+    msg = f"Invalid Data: {path} line: {number}"
     with pytest.warns() as record:
         assert not Project.is_valid(path)
         assert len(record) == 1
