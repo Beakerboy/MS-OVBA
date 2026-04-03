8d9c0aea27f382e33c619a9c3431341a3130a096
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-03 14:27:08 -0400

    Update test_dir.py

--- tests/Unit/Views/test_dir.py
+++ tests/Unit/Views/test_dir.py
@@ -1,4 +1,6 @@
+from ms_cfb.ole_file import OleFile
 from ms_ovba.Views.dirStream import DirStream
+from ms_ovba_compression.ms_ovba import MsOvba
 from unittest import mock
 
 
@@ -8,3 +10,17 @@ mock_vbaproject = mock.Mock()
 def test_construct() -> None:
     dir = DirStream(mock_vbaproject)
     assert isinstance(dir, DirStream)
+
+
+def test_is_valid() -> None:
+    file = "test/blank/vbaProject.bin"
+    ole_file = OleFile.create_from_file(file)
+    ole_file.extract_stream('dir', 'test/blank/')
+    with open('test/blank/dir.bin', 'rb' as f:
+        compressed_data = f.read()
+
+        # Use MsOvba to decompress the stream
+        ms_ovba = MsOvba()
+        decompressed_data = ms_ovba.decompress(compressed_data)
+        assert DirStream.is_valid(f)
+    
