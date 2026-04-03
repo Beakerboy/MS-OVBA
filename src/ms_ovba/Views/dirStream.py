eb2df34c270d6edf0cb7f4a23d0a527d8678a3d4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-03 16:52:40 -0400

    Update dirStream.py

--- src/ms_ovba/Views/dirStream.py
+++ src/ms_ovba/Views/dirStream.py
@@ -221,7 +221,7 @@ class DirStream():
         return project_data
 
     @staticmethod
-    def _parse_reference_group(data, offset, pack_symbol):
+    def _parse_reference_group(data: bytes, offset: int, pack_symbol: str):
         """Consumes records until the end of a single Reference definition."""
         # A Reference is a cluster of records (Name, Libid, etc.)
         # Logic: Consume the first record, then peek for optional sub-records 
@@ -233,7 +233,7 @@ class DirStream():
         return record_content, offset + 6 + size
 
     @staticmethod
-    def _parse_module_group(data, offset, pack_symbol):
+    def _parse_module_group(data: bytes, offset: int, pack_symbol: str):
         """Consumes all records for one Module until the 0x002B terminator."""
         module_bytes = b''
         while offset < len(data):
@@ -241,6 +241,6 @@ class DirStream():
             record_total_len = 6 + size
             module_bytes += data[offset : offset + record_total_len]
             offset += record_total_len
-            if r_id == 0x002B: # MODULE Terminator
+            if r_id == 0x002B:  # MODULE Terminator
                 break
         return module_bytes, offset
