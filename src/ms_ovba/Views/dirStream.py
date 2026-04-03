916c7253bdeb90323e0b0f33133bd334fb110850
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-03 17:03:17 -0400

    Update dirStream.py

--- src/ms_ovba/Views/dirStream.py
+++ src/ms_ovba/Views/dirStream.py
@@ -1,6 +1,7 @@
 import struct
 from ms_ovba_compression.ms_ovba import MsOvba
 from ms_ovba.vbaProject import VbaProject
+from ms_ovba.Models.Entities.reference_record import ReferenceRecord
 from ms_ovba.Models.Fields.idSizeField import IdSizeField
 from ms_ovba.Models.Fields.doubleEncodedString import (
     DoubleEncodedString
