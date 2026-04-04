c0d8e3ab980ef2a3d334618321bcca0d9a3b786d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-04 13:26:51 -0400

    Update dirStream.py

--- src/ms_ovba/Views/dirStream.py
+++ src/ms_ovba/Views/dirStream.py
@@ -237,6 +237,7 @@ class DirStream():
             ref = Reference.unpack(
                 data[offset:offset + record_size], "little")
             project_data["references"] += [ref]
+            offset += record_size
             record_id, size = struct.unpack_from("<HI", data, offset)
 
         if record_id != 0x0f or size != 2:
