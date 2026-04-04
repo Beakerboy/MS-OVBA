5163e93e147c48bc2bbe5ea7276b46e13261587e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-04 10:22:09 -0400

    Update dirStream.py

--- src/ms_ovba/Views/dirStream.py
+++ src/ms_ovba/Views/dirStream.py
@@ -164,7 +164,8 @@ class DirStream():
         value, r2, s2, v2 = struct.unpack_from(
             f"{size}sHI{2*size}s", data, offset + 6)
         if record_id != 5 or size > 2000 or r2 != 0x40 or s2 != 2 * size:
-            raise ValueError("Incorrect PROJECTDOCSTRING")
+            raise ValueError(
+                "Incorrect PROJECTDOCSTRING({record_id}, {size}, {r2}, {s2})")
         offset += 12 + size * 3
 
         record_id, size = struct.unpack_from("<HI", data, offset)
