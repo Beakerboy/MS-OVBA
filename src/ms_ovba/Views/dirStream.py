7e60d60c0f1f0e5fbec8acac4e2a6b8b79b3d6f8
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-04 17:02:43 -0400

    Update dirStream.py

--- src/ms_ovba/Views/dirStream.py
+++ src/ms_ovba/Views/dirStream.py
@@ -262,10 +262,15 @@ class DirStream():
             record_id, size, = struct.unpack_from(
                 "<HI", data, offset + record_size)
             record_size += 6
-            value, record_id, size = struct.unpack_from(
-                f"<{size}sHI", data, offset + record_size)
+            if record_id != 0x1a:
+                raise ValueError(f"Incorrect MODULESTREAMNAME({record_id})")
+            value, record_id, size, value = struct.unpack_from(
+                f"<{size}sHI{size*2}s", data, offset + record_size)
             module_data["stream_name"] = value
-            record_size += size + 6
+            record_size += size * 3 + 6
+            record_id, size, = struct.unpack_from(
+                "<HI", data, offset + record_size)
+            record_size += 6
             if record_id != 0x1c:
                 raise ValueError(f"Incorrect MODULEDOCSTRING({record_id})")
             value, record_id, size, value2 = struct.unpack_from(
