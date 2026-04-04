643aa57904ad055ebfc1db8099c2e7a81a70315f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-04 10:04:46 -0400

    Update dirStream.py

--- src/ms_ovba/Views/dirStream.py
+++ src/ms_ovba/Views/dirStream.py
@@ -236,7 +236,7 @@ class DirStream():
                     raise ValueError("Unknown Reference Type")
             ref = ReferenceRecord.unpack(
                 data[offset:offset + record_size], "little")
-            project_data["references"] += ref
+            project_data["references"] += [ref]
             record_id, size = struct.unpack_from("<HI", data, offset)
 
         if record_id != 0x0f or size != 2:
