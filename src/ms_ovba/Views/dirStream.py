ec8531b6084156bd6f07a5a0fe33b02a79684ca7
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-07 11:01:07 -0400

    Update dirStream.py

--- src/ms_ovba/Views/dirStream.py
+++ src/ms_ovba/Views/dirStream.py
@@ -162,8 +162,9 @@ class DirStream():
         if record_id == 0x4A:
             if size != 4:
                 raise ValueError("Incorrect PROJECTCOMPATVERSION")
-            record_id, size, value = stream.read_id_size_val()
             project_data["compatversion"] = int_value
+            record_id, size, value = stream.read_id_size_val()
+            int_value = int.from_bytes(value, byteorder='little')
         
         if record_id != 2 or size != 4 or int_value != 0x409:
             raise ValueError(f"Incorrect PROJECTLCID({record_id}), {size}, {int_value}")
