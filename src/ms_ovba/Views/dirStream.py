04f0a276fd846b246a63181ba85268d5217c6e48
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-07 15:37:56 -0400

    Update dirStream.py

--- src/ms_ovba/Views/dirStream.py
+++ src/ms_ovba/Views/dirStream.py
@@ -202,7 +202,7 @@ class DirStream():
         record_id, size, value = stream.read_id_size_val()
         if record_id != 7 or size != 4:
             raise ValueError("Incorrect PROJECTHELPCONTEXT")
-       project_data["helpcontext"] = int.from_bytes(value, byteorder='little')
+        project_data["helpcontext"] = int.from_bytes(value, byteorder='little')
 
         record_id, size, value = stream.read_id_size_val()
         int_value = int.from_bytes(value, byteorder='little')
