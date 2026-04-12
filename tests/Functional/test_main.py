7352313fbc435d69676bff2a1384e83ec62691dc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-11 22:05:19 -0400

    Update test_main.py

--- tests/Functional/test_main.py
+++ tests/Functional/test_main.py
@@ -8,7 +8,7 @@ def test_main(mocker: MockerFixture) -> None:
 
     olefile = OleFile.create_from_file("vbaProject.bin")
     olefile.extract_stream("Sheet1")
-    with open("Sheet1.bin", "rb") as file
+    with open("Sheet1.bin", "rb") as file:
         contents = file.read()
     compressor = MsOvba()
     uncompressed = compressor.decompress(contents)
