c4784058dacecc30e40c324c8b29e9428f6dcc80
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-12 10:38:46 -0400

    Update __main__.py

--- src/ms_ovba/__main__.py
+++ src/ms_ovba/__main__.py
@@ -34,9 +34,9 @@ def main() -> None:
     sheet1 = DocModule('Sheet1')
     base_path = os.path.dirname(__file__)
     sheet1.add_file(base_path + '/blank_files/Sheet1.cls')
-    sheet1.normalize_file()
     guid1 = uuid.UUID("0002082000000000C000000000000046")
     sheet1.add_guid(guid1)
+    sheet1.normalize_file()
     project.add_module(sheet1)
 
     workbook = DocModule('ThisWorkbook')
