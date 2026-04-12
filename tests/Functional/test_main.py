2eef4cc123f6c99e72d80d1c56f97210f6d4a009
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-12 10:12:43 -0400

    Update test_main.py

--- tests/Functional/test_main.py
+++ tests/Functional/test_main.py
@@ -21,7 +21,7 @@ def test_main(mocker: MockerFixture) -> None:
     compressor = MsOvba()
     uncompressed = compressor.decompress(contents)
     expected = ('Attribute VB_Name = "Sheet1"\r\n'
-        'Attribute VB_Base = "0\r\n'
+        'Attribute VB_Base = "0{00020820-0000-0000-C000-000000000046}\r\n'
         'Attribute VB_GlobalNameSpace = False\r\n'
         'Attribute VB_Creat... = True\r\n'
         'Attribute VB_Exposed = True\r\n'
