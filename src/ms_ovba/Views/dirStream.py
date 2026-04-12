223078e2b0c4a401aedec661e22ca58fac64ddd4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-04-12 10:23:49 -0400

    Update dirStream.py

--- src/ms_ovba/Views/dirStream.py
+++ src/ms_ovba/Views/dirStream.py
@@ -167,7 +167,9 @@ class DirStream():
             int_value = int.from_bytes(value, byteorder='little')
 
         if record_id != 2 or size != 4 or int_value != 0x409:
-            raise ValueError(f"Incorrect PROJECTLCID({record_id}), {size}, {int_value}")
+            raise ValueError(
+                f"Incorrect PROJECTLCID({record_id}), {size}, {int_value}"
+            )
 
         record_id, size, value = stream.read_id_size_val()
         int_value = int.from_bytes(value, byteorder='little')
